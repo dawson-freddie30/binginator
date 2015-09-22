6a189e5979d688478ebf4ab7e5b84a1b0236edfb
Freddie Dawson <dawson.freddie30@outlook.com>
2015-09-22 01:52:15 -0400

    add icon and autoupdating

--- Project/Binginator/App.xaml.cs
+++ Project/Binginator/App.xaml.cs
@@ -1,22 +1,48 @@
-﻿using Binginator.Windows;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Reflection;
 using System.Windows;
+using Binginator.Windows;
 
 namespace Binginator {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+
+    // icon from https://www.iconfinder.com/icons/670456/bing_communication_media_question_search_social_icon#size=64
     public partial class App : Application {
         public static string Folder = AppDomain.CurrentDomain.BaseDirectory;
         private void _Startup(object sender, StartupEventArgs e) {
             if (!File.Exists(Path.Combine(App.Folder, "chromedriver.exe")))
                 new MsgWindow("Unable to locate required files. Reinstall this program.").ShowDialog();
             else {
-                AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(_AssemblyResolve);
-                new MainWindow().Show();
+                ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+                MsgWindow msgWindow = new MsgWindow("Checking for an update...");
+                msgWindow.Show();
+
+                string latest = null;
+                try {
+                    latest = new WebClient().DownloadString("https://raw.githubusercontent.com/dawson-freddie30/binginator/release/latest").TrimEnd(new char[] { '\r', '\n' });
+                }
+                catch (WebException ex) {
+                    new MsgWindow("Update checking failed." + Environment.NewLine + Environment.NewLine + ex.Message).ShowDialog();
+                }
+
+                msgWindow.Close();
+
+                string current = "v" + new Version(FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVer‌​sion).ToString(2);
+                if (latest != null && current != latest) {
+                    Process.Start(new ProcessStartInfo("msiexec.exe", "/I https://github.com/dawson-freddie30/binginator/releases/download/" + latest + "/Binginator.msi LAUNCH=1"));
+                    Shutdown();
+                }
+                else {
+                    ShutdownMode = ShutdownMode.OnLastWindowClose;
+                    AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(_AssemblyResolve);
+                    new MainWindow().Show();
+                }
             }
         }
 
